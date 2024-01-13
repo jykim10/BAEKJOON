e356274103f39d168a4f3ad11a4c3f09019e7254
jykim10 <156491868+jykim10@users.noreply.github.com>
2024-01-13 13:40:20 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+if t>89 and t<=100: 
+  print('A')
+elif t>79 and t<=89: 
+  print('B')
+elif t>69 and t<=79:
+  print('C')
+elif t>59 and t<=69:
+  print('D')
+else: 
+  print('F')
