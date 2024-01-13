0a8c21bd9f1b872e825d6ff405ff2e7e8c1d7222
jykim10 <156491868+jykim10@users.noreply.github.com>
2024-01-13 13:07:05 +0900

    [Bronze IV] Title: 전자레인지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.py
@@ -0,0 +1,13 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+
+if A<=0:
+  time = abs(C*A) + D + (B*E)
+  print(time)
+
+else:
+  time = (B-A) * E
+  print(time)
